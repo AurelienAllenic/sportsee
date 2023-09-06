38caa4318db2650c403edcf41487e3fee289a170
AurelienAllenic <aurelien.allenic@gmail.com>
2023-09-06 14:54:52 +0200

    (feat) added right behaviour for averageSessions chart with hovering grey, now i have to add the rrsponsive

--- front/src/components/dashboard/Average.jsx
+++ front/src/components/dashboard/Average.jsx
@@ -1,6 +1,28 @@
 import React, { useEffect, useState, useRef} from 'react';
 import ApiService from './apiService';
-import { LineChart, XAxis, YAxis, Tooltip, Line, Area, ReferenceLine, ReferenceArea } from 'recharts';
+import { LineChart, XAxis, YAxis, Tooltip, Line, Rectangle } from 'recharts';
+
+function customMouseMove(e){
+  let sessionWrap = document.querySelector('.container_average_sessions');
+
+  if (e.isTooltipActive) {
+    let windowWidth = sessionWrap.offsetWidth;
+    let mouseXpercent = Math.floor(
+      (e.activeCoordinate.x / windowWidth) * 100
+    );
+    
+    sessionWrap.style.background = `linear-gradient(90deg, rgba(255,0,0, 1) ${mouseXpercent}%, rgba(0,0,0,0.1) ${mouseXpercent}%, rgba(0,0,0,0.1) 100%)`;
+    sessionWrap.style.borderRadius = '10px';
+  }
+  else{
+      sessionWrap.style.background ='transparent'
+  }
+}
+
+function customOnMouseOut(){
+  let sessionWrap = document.querySelector('.container_average_sessions');
+  sessionWrap.style.background ='transparent'
+}
 
 const Average = ({ userId }) => {
   const [infosAverage, setInfosAverage] = useState([]);
@@ -11,43 +33,6 @@ const Average = ({ userId }) => {
   const [endIndex, setEndIndex] = useState(null);
   const [showReferenceLine, setShowReferenceLine] = useState(false);
 
-  const handleMouseEnter = (data, index) => {
-    console.log('entre')
-    const lineChartElement = document.getElementById('linechart');
-    const tooltipElement = document.createElement('div');
-    tooltipElement.className = 'custom-tooltip-average';
-    tooltipElement.style.position = 'absolute';
-  
-    // Get the coordinates and dimensions of the chart
-    const lineChartRect = lineChartElement.getBoundingClientRect();
-  
-    // Adjust the style of the tooltip element
-    tooltipElement.style.top = `${lineChartRect.top}px`;
-    tooltipElement.style.left = `${lineChartRect.left}px`;
-    tooltipElement.style.width = `${lineChartRect.width}px`;
-    tooltipElement.style.height = `${lineChartRect.height}px`;
-  
-    // Set the tooltip content
-    const sessionLength = data.sessionLength;
-    tooltipElement.innerHTML = `<span>${sessionLength} min</span>`;
-  
-    lineChartElement.appendChild(tooltipElement);
-    setEndIndex(index);
-  setShowReferenceLine(true);
-  };
-  
-  
-  
-  const handleMouseLeave = () => {
-    const lineChartElement = document.getElementById('linechart');
-    const tooltipElement = lineChartElement.getElementsByClassName('custom-tooltip-average')[0];
-    lineChartElement.removeChild(tooltipElement);
-    setEndIndex(null);
-  setShowReferenceLine(false);
-  };
-  
-
-  
   useEffect(() => {
     ApiService.getUserAverageSession(userId)
       .then((res) => {
@@ -67,14 +52,27 @@ const Average = ({ userId }) => {
   }, []);
   
   
-
+  const CustomCursor = ({points}) => {
+    const X = points[0].x
+    const Y = points[0].y
+  
+    return (
+      <Rectangle
+        width={1000}
+        height={1000}
+        x={X}
+        y={Y - 100}
+        style={{background: 'red', opacity: 0.1}}
+      />
+    )
+  }
   
 const CustomTooltipAverage = ({ active, payload }) => {
   if (active && payload && payload.length) {
     const sessionLength = payload[0].value;
 
     return (
-      <div className="custom-tooltip-average" onMouseEnter={() => handleMouseEnter(payload[0].payload)}>
+      <div className="custom-tooltip-average">
         <span>{sessionLength} min</span>
       </div>
     );
@@ -93,8 +91,7 @@ const CustomTooltipAverage = ({ active, payload }) => {
 
   return (
     <>
-    <div           onMouseMove={handleMouseEnter}
-          onMouseLeave={handleMouseLeave}>
+    <div className='container_average_sessions'>
       <LineChart
         width={350}
         height={350}
@@ -102,38 +99,41 @@ const CustomTooltipAverage = ({ active, payload }) => {
         margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
         style={{ background: 'red', borderRadius: '10px' }}
         id='linechart'
-        
+        onMouseMove={(e) => customMouseMove(e)}
+                    onMouseOut={() => customOnMouseOut()}
       >
         <text x={10} y={10} dx={10} dy={50} style={{ fontSize: '20px', fill: 'white', opacity: 0.8 }}>
           Durée moyenne des <tspan x={10} y={65} dx={10} dy={25}>sessions</tspan>
         </text>
   
         <XAxis
-  dataKey="day"
-  tickFormatter={formatDayTick}
-  axisLine={false}
-  tickLine={false}
-  tick={{ fill: 'white', opacity: 0.8 }}
-  padding={{ left: -30, right: -30 }}
-/>
+          dataKey="day"
+          tickFormatter={formatDayTick}
+          axisLine={false}
+          tickLine={false}
+          tick={{ fill: 'white', opacity: 0.8, margin: 50 }}
+          padding={{ left: -40, right: -40 }}
+          textAnchor="middle"
+        />
 
         <YAxis dataKey="sessionLength" hide={true} data={infosAverage} domain={yAxisDomain} scale={'linear'}/>
   
         <Tooltip
-  label=""
-  content={<CustomTooltipAverage />}
-  wrapperStyle={{ background: 'black' }}
-  itemStyle={{ color: 'black' }}
-  offset={10}
-/>
-<ReferenceArea
-          x1={startIndex}
-          x2={endIndex}
-          y1={0}
-          y2={Math.max(...infosAverageSessionLength)}
-          fill="#cccccc"
-          fillOpacity={0.5}
-        />
+            wrapperStyle={{
+              background: '#FFF',
+              color: '#000',
+              width: '75px',
+              height: '45px',
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+              textAlign: 'center',
+              outline: 'none',
+            }}
+            labelStyle={{display: 'none', border: 'none'}}
+            content={<CustomTooltipAverage />}
+            cursor={<CustomCursor />}
+          />
   
         <Line
           type="monotone"
